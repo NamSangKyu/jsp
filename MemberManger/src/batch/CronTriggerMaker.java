4f00390848419d969c14d110b673c260ed3ea0dc
nam2626 <nam2626@gmail.com>
2021-01-13 17:57:15 +0900

    자동 커밋

--- MemberManger/src/batch/CronTriggerMaker.java
+++ MemberManger/src/batch/CronTriggerMaker.java
@@ -51,7 +51,28 @@ public class CronTriggerMaker {
 		CronTriggerMaker js = new CronTriggerMaker("* * * * * ?", IntervalJob.class);
 		js.createTrigger();
 	}
-	
+	/*
+	 * 크론 표현식 : 초 분 시 일 월 요일 년도
+	 * 	초 : 0~59,   * , /
+	 * 	분 : 0~59,   * , /
+	 *	시 : 0~23,   * , /
+	 * 	일 : 1~31,   * , / L W ?
+	 * 	월 : 1~12,   * , /
+	 *  요일 : 0~6 , SUN ~ SAT    , * ? / L #
+	 *  년도 : 비우거나 1970 ~ 2099  , - *  /
+	 *     
+	 *     
+	 *  * : 모든 값
+	 *  ? : 특정한 값이 없음
+	 *  - : 범위   SUN-SAT
+	 *  / : 시작시간 / 단위
+	 *  , : 특정 값일때만
+	 *  
+	 *  0/5 * * * * ?  --> 5초마다
+	 *  0 0/5 * * * ?  --> 5분 마다
+	 *  0 22 23 ? * *  --> 23시 22분 0초 마다 실행
+	 *  http://www.cronmaker.com/
+	 */
 	
 }
 
