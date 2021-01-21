b2d653706a4a980f32ca95b124748abb7f61095b
nam2626 <nam2626@gmail.com>
2021-01-21 17:37:45 +0900

    자동 커밋

--- BoardJSPFinal/src/dao/MemberDAO.java
+++ BoardJSPFinal/src/dao/MemberDAO.java
@@ -25,41 +25,11 @@ public class MemberDAO {
 	}
 
 	public MemberVO selectMemberVO(String id) {
-		MemberVO vo = null;
-		String sql = "select * from member where id like ?";
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			pstmt = DBManager.getInstance().getConn().prepareStatement(sql);
-			pstmt.setString(1, id);
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				vo = new MemberVO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			DBManager.getInstance().close(pstmt, rs);
-		}
-		
-		return vo;
+		return session.selectOne("member.selectMemberVO", id);
 	}
 	
-	public void insertMemberVO(MemberVO vo) throws MemberException {
-		PreparedStatement pstmt = null;
-		String sql = "insert into member(id,pass,name,age) values(?,?,?,?)";
-		try {
-			pstmt = DBManager.getInstance().getConn().prepareStatement(sql);
-			pstmt.setString(1, vo.getId());
-			pstmt.setString(2, vo.getPass());
-			pstmt.setString(3, vo.getName());
-			pstmt.setInt(4, vo.getAge());
-			int count = pstmt.executeUpdate();
-			if(count == 0)
-				throw new MemberException("회원등록에 실패했습니다.");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	public void insertMemberVO(MemberVO vo) {
+		session.insert("member.insertMemberVO", vo);
 	}
 
 	public void updatePass(String id, String pass) throws MemberException {
